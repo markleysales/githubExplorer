9d3863bc4235eb2c346e84102f89871d56f20143
Markley <marksales.dev.eng@gmail.com>
2022-03-14 20:23:20 -0300

    sixthcommit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV !== "production"; 
 
@@ -22,16 +23,24 @@ module.exports = {
         hot: "only",
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "public", "index.html")
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules:[
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: "babel-loader",
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve("react-refresh/babel")
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
